588c4f809d15c2f3e87108260d825e119dd742e6
Rogerio Tadim <rogerio.tadim@dataprev.gov.br>
2025-01-23 14:55:21 +0000

    fix image & revert cep usecase

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM golang:1.22.8 as builder
 WORKDIR /app
 COPY . .
-RUN go build -o cloudrun cmd/api/main.go
+RUN go build -o cloudrun cmd/api/main.go && chmod +x cloudrun
 
-FROM ubuntu
+FROM debian:latest
+RUN apt-get update && apt-get install -y ca-certificates
 WORKDIR /app
-COPY --from=builder /app/cloudrun /app/cloudrun
-CMD [ "/app/cloudrun" ]
+COPY --from=builder /app/cloudrun .
+ENTRYPOINT [ "./cloudrun" ]
 
--- internal/usecase/cep.go
+++ internal/usecase/cep.go
@@ -3,11 +3,9 @@ package usecase
 import (
 	"context"
 	"encoding/json"
-	"io"
+	"errors"
 	"log/slog"
-	"net"
 	"net/http"
-	"time"
 
 	lab "github.com/goexpert/labobservabilidade"
 )
@@ -19,53 +17,27 @@ func GetLogradouro(ctx context.Context, cep lab.CepDto, client *http.Client) (*l
 
 	var logradouro lab.LogradouroDto
 
-	var netTransport = &http.Transport{
-		Dial: (&net.Dialer{
-			Timeout: 5 * time.Second,
-		}).Dial,
-		TLSHandshakeTimeout: 5 * time.Second,
-	}
-	var netClient = &http.Client{
-		Timeout:   time.Second * 10,
-		Transport: netTransport,
-	}
-	response, err := netClient.Get(_url)
+	wClient, err := lab.NewWebclient(ctx, client, http.MethodGet, _url, nil)
 	if err != nil {
-		slog.Error("erro na requisição", "error", err.Error())
+		slog.Error("falha na req para o OpenCep", "error", err.Error())
 		return nil, err
 	}
-	defer response.Body.Close()
 
-	body, err := io.ReadAll(response.Body)
+	err = wClient.Do(func(p []byte) error {
+		err = json.Unmarshal(p, &logradouro)
+		if err != nil {
+			slog.Error("erro no unmarshal do cep", "error", err.Error())
+		}
+		return err
+	})
 	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(body, &logradouro)
-	if err != nil {
-		slog.Error("erro no unmarshal do cep", "error", err.Error())
+		slog.Error("executa webclient", "error", err.Error())
+
 	}
 
-	// wClient, err := lab.NewWebclient(ctx, client, http.MethodGet, _url, nil)
-	// if err != nil {
-	// 	slog.Error("falha na req para o OpenCep", "error", err.Error())
-	// 	return nil, err
-	// }
-
-	// err = wClient.Do(func(p []byte) error {
-	// 	err = json.Unmarshal(p, &logradouro)
-	// 	if err != nil {
-	// 		slog.Error("erro no unmarshal do cep", "error", err.Error())
-	// 	}
-	// 	return err
-	// })
-	// if err != nil {
-	// 	slog.Error("executa webclient", "error", err.Error())
-
-	// }
-
-	// if logradouro.Erro != "" {
-	// 	return nil, errors.New("cep não encontrado")
-	// }
+	if logradouro.Erro != "" {
+		return nil, errors.New("cep não encontrado")
+	}
 
 	return &logradouro, err
 }
